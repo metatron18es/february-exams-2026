b025558d62bc663fcc5d00ec994ac94f9d6a745f
Roberto de la Casa <roberto.delacasa@opositatest.com>
2026-01-26 10:37:48 +0100

    feat: add mobile support

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const checkboxes = document.querySelectorAll('.filters input');
 checkboxes.forEach(cb => {
     cb.addEventListener('change', () => {
         document.querySelectorAll('.' + cb.value).forEach(event => {
-            event.style.display = cb.checked ? 'block' : 'none';
+            event.classList.toggle('hidden', !cb.checked);
         });
     });
 });
